ee338cc2d0d84a6266e8e845bd08759566acdc52
Vlamonster <jellemartijndijkstra@gmail.com>
2023-01-12 16:40:36 +0100

    Fix small error introduced when resolving clippy::pedantic warnings.

--- src/solver/depth_first_search.rs
+++ src/solver/depth_first_search.rs
@@ -70,11 +70,12 @@ pub fn solve(stdout: &mut Stdout, maze: &mut Maze, delay: u64, trace: bool) -> R
     for (&(x, y), &(nx, ny)) in unvisited.iter().tuple_windows() {
         sleep(Duration::from_millis(delay));
 
-        let (dx, dy) = (if nx > x { 1 } else { -1 }, if ny > y { 1 } else { -1 });
-
         // Print arrow pointing to neighbor in current cell.
         #[rustfmt::skip]
-        let dir = match (dx, dy) {
+        let dir = match (
+            if nx > x { 1 } else if nx < x { -1 } else { 0 },
+            if ny > y { 1 } else if ny < y { -1 } else { 0 },
+        ) {
             ( 1,  _) => '→',
             (-1,  _) => '←',
             ( _,  1) => '↓',
